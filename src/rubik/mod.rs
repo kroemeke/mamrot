052657c173f14a7d95f3bdce88ef9a02ecb1e844
Marek Kroemeke <git@kroemeke.uk>
2024-07-03 22:12:04 +0100

    Add some basic usage or rand
    Just playing with generating ranodm numbers

--- src/rubik/mod.rs
+++ src/rubik/mod.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 // Typical buffer sizes
 static magic_numbers: [u64; 11] = [32,64,128,256,512,1024,2048,4096,8192,32767,65535];
 
@@ -16,7 +18,13 @@ pub struct Cube {
 
 impl Cube {
     pub fn new() -> Cube {
+        let mut rng = rand::thread_rng();
         Cube {
+            int_size_1: rng.gen_range(1..=10),
+            int_size_2: rng.gen_range(1..=1000),
+            int_size_3: rng.gen_range(1..=100000),
+            int_size_4: rng.gen_range(1..=10000000),
+            int_size_5: rng.gen_range(1..=1000000000),
             ..Default::default()
         }
     }
